f70ed1d25ff7f277fec447b06267634725f4fe9e
Sebastian-ivaniczki <sivaniczki@gmail.com>
2022-12-22 16:25:47 +0100

    add img

--- js/script.js
+++ js/script.js
@@ -1,23 +1,53 @@
+//! operazioni preliminari
+const galleryElement = document.getElementById('gallery');
+const next = document.getElementById('"arrow-right');
+const prev = document.getElementById('"arrow-left');
+// recupero le immagini
+
+//* array 
 const data = [
-    {
-      image: 'img/01.webp',
-      title: "Marvel\'s Spiderman Miles Morale",
-      text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-    }, {
-      image: 'img/02.webp',
-      title: 'Ratchet & Clank: Rift Apart',
-      text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-    }, {
-      image: 'img/03.webp',
-      title: 'Fortnite',
-      text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-    }, {
-      image: 'img/04.webp',
-      title: 'Stray',
-      text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-    }, {
-      image: 'img/05.webp',
-      title: "Marvel's Avengers",
-      text: "Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
-    }
-  ];
+  {
+    image: 'img/01.webp',
+    title: "Marvel\'s Spiderman Miles Morale",
+    text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+  }, {
+    image: 'img/02.webp',
+    title: 'Ratchet & Clank: Rift Apart',
+    text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+  }, {
+    image: 'img/03.webp',
+    title: 'Fortnite',
+    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+  }, {
+    image: 'img/04.webp',
+    title: 'Stray',
+    text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+  }, {
+    image: 'img/05.webp',
+    title: "Marvel's Avengers",
+    text: "Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+  }
+];
+
+
+let activeIndex = 0;
+
+// ciclo per girare nei elementi dell'aray
+let carosel = '';
+for(let i = 0; i < data.length; i++){
+  const dataField = data[i]
+  
+  carosel += ` 
+    <img src="${dataField.image}" alt="${dataField.title}">
+    <div>
+      <h3>${dataField.title}</h3>
+      <p>${dataField.text}</p>
+    </div>
+    ` 
+}
+galleryElement.innerHTML += carosel;  
+const img = document.querySelectorAll('#gallery img');
+const description = document.querySelectorAll('#gallery div');
+
+img[activeIndex].classList.add('active');
+description[activeIndex].classList.add('active');
